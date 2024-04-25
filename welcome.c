3a1d0ed881bc8e85fc472099f5508915d1f10a4f
Sabin077 <080bct077@ioepc.edu.np>
2024-04-25 16:43:57 +0545

    Final commit

--- welcome.c
+++ welcome.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include <time.h>
 void welcome(){
+    printf("\n\n\n\n\n\n");
     printf("\t\t\t\t\t _________    |               _________      _________     |    /\n");
     printf("\t\t\t\t\t/             |              /         \\    /              |   /\n");
     printf("\t\t\t\t\t|             |              |         |    |              |  / \n");
